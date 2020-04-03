cdd67e6c887299a8ba15d22cbef31cb2e5cd9d97
Janak Amarasena <janakamarasena@gmail.com>
2020-04-03 12:31:23 +0530

    Add sp role mapping support when using associated local users

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/handler/request/impl/PostAuthAssociationHandler.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/handler/request/impl/PostAuthAssociationHandler.java
@@ -24,6 +24,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.ApplicationAuthenticator;
 import org.wso2.carbon.identity.application.authentication.framework.FederatedApplicationAuthenticator;
+import org.wso2.carbon.identity.application.authentication.framework.config.model.ApplicationConfig;
 import org.wso2.carbon.identity.application.authentication.framework.config.model.AuthenticatorConfig;
 import org.wso2.carbon.identity.application.authentication.framework.config.model.SequenceConfig;
 import org.wso2.carbon.identity.application.authentication.framework.config.model.StepConfig;
@@ -32,12 +33,16 @@ import org.wso2.carbon.identity.application.authentication.framework.exception.F
 import org.wso2.carbon.identity.application.authentication.framework.exception.PostAuthenticationFailedException;
 import org.wso2.carbon.identity.application.authentication.framework.handler.request.AbstractPostAuthnHandler;
 import org.wso2.carbon.identity.application.authentication.framework.handler.request.PostAuthnHandlerFlowStatus;
+import org.wso2.carbon.identity.application.authentication.framework.handler.sequence.impl.DefaultSequenceHandlerUtils;
 import org.wso2.carbon.identity.application.authentication.framework.internal.FrameworkServiceDataHolder;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
 import org.wso2.carbon.identity.application.authentication.framework.util.FrameworkConstants;
 import org.wso2.carbon.identity.application.authentication.framework.util.FrameworkErrorConstants;
 import org.wso2.carbon.identity.application.authentication.framework.util.FrameworkUtils;
+import org.wso2.carbon.identity.application.common.model.ClaimConfig;
 import org.wso2.carbon.identity.application.common.model.ClaimMapping;
+import org.wso2.carbon.identity.claim.metadata.mgt.ClaimMetadataHandler;
+import org.wso2.carbon.identity.claim.metadata.mgt.exception.ClaimMetadataException;
 import org.wso2.carbon.identity.user.profile.mgt.UserProfileAdmin;
 import org.wso2.carbon.identity.user.profile.mgt.UserProfileException;
 import org.wso2.carbon.identity.user.profile.mgt.association.federation.FederatedAssociationManager;
@@ -48,8 +53,10 @@ import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import static org.wso2.carbon.identity.application.authentication.framework.handler.request.PostAuthnHandlerFlowStatus.SUCCESS_COMPLETED;
 
@@ -157,7 +164,10 @@ public class PostAuthAssociationHandler extends AbstractPostAuthnHandler {
         sequenceConfig.setAuthenticatedUser(
                 AuthenticatedUser.createLocalAuthenticatedUserFromSubjectIdentifier(fullQualifiedAssociatedUserId));
         sequenceConfig.getApplicationConfig().setMappedSubjectIDSelected(true);
-        Map<ClaimMapping, String> authenticatedUserAttributes = getClaimMapping(stepConfig, context);
+
+        Map<String, String> mappedAttrs = handleClaimMappings(stepConfig, context);
+        handleRoleMapping(context, sequenceConfig, mappedAttrs);
+        Map<ClaimMapping, String> authenticatedUserAttributes = getClaimMapping(context, mappedAttrs);
         if (MapUtils.isNotEmpty(authenticatedUserAttributes)) {
             sequenceConfig.getAuthenticatedUser().setUserAttributes(authenticatedUserAttributes);
             if (log.isDebugEnabled()) {
@@ -227,25 +237,15 @@ public class PostAuthAssociationHandler extends AbstractPostAuthnHandler {
     /**
      * To get the claim mapping based on user local.
      *
-     * @param stepConfig Step Config.
      * @param context    Authentication Context.
+     * @param mappedAttrs    Mapped user attributes.
      * @return claim mapping.
      * @throws PostAuthenticationFailedException Post Authentication Failed Exception.
      */
     @SuppressWarnings("unchecked")
-    private Map<ClaimMapping, String> getClaimMapping(StepConfig stepConfig, AuthenticationContext context)
-            throws PostAuthenticationFailedException {
+    private Map<ClaimMapping, String> getClaimMapping(AuthenticationContext context, Map<String, String> mappedAttrs) {
 
-        Map<String, String> mappedAttrs;
         Map<ClaimMapping, String> mappedClaims = null;
-        try {
-            mappedAttrs = FrameworkUtils.getClaimHandler().handleClaimMappings(stepConfig, context, null, false);
-        } catch (FrameworkException e) {
-            throw new PostAuthenticationFailedException(FrameworkErrorConstants.ErrorMessages.
-                    ERROR_WHILE_GETTING_CLAIM_MAPPINGS.getCode(), String.format(FrameworkErrorConstants.ErrorMessages.
-                            ERROR_WHILE_GETTING_CLAIM_MAPPINGS.getMessage(),
-                    context.getSequenceConfig().getAuthenticatedUser().getUserName()), e);
-        }
         Map<String, String> localClaimValues = (Map<String, String>) context
                 .getProperty(FrameworkConstants.UNFILTERED_LOCAL_CLAIM_VALUES);
         Map<String, String> idpClaimValues = (Map<String, String>) context
@@ -263,5 +263,151 @@ public class PostAuthAssociationHandler extends AbstractPostAuthnHandler {
             mappedClaims = FrameworkUtils.buildClaimMappings(mappedAttrs);
         }
         return mappedClaims;
+    }
+
+
+    private void handleRoleMapping(AuthenticationContext context, SequenceConfig sequenceConfig, Map<String, String>
+            mappedAttrs) throws PostAuthenticationFailedException {
+
+        String spRoleUri = getSpRoleClaimUri(sequenceConfig.getApplicationConfig());
+        String roleAttr = null;
+        String spStandardDialect = getSPStandardDialect(context);
+        if (spStandardDialect != null && isLocalClaimDialect(context)) {
+            spRoleUri = getStandardRoleClaimURI(spStandardDialect, context.getTenantDomain());
+            roleAttr = mappedAttrs.get(spRoleUri);
+        } else if (spStandardDialect != null && !isLocalClaimDialect(context)) {
+            String localClaim = getSPMappedLocalRoleClaimURI(sequenceConfig.getApplicationConfig());
+            spRoleUri = getStandardClaimURIFromLocal(spStandardDialect, context.getTenantDomain(),
+                    localClaim);
+            roleAttr = mappedAttrs.get(spRoleUri);
+        } else if (spStandardDialect == null && isLocalClaimDialect(context)) {
+            roleAttr = mappedAttrs.get(spRoleUri);
+        } else if (spStandardDialect == null && !isLocalClaimDialect(context)) {
+            roleAttr = mappedAttrs.get(spRoleUri);
+        }
+
+        if (StringUtils.isNotBlank(roleAttr)) {
+            //Need to convert multiAttributeSeparator value into a regex literal before calling
+            // split function. Otherwise split can produce misleading results in case
+            // multiAttributeSeparator contains regex special meaning characters like .*
+            String[] roles = roleAttr.split(Pattern.quote(FrameworkUtils.getMultiAttributeSeparator()));
+            String spMappedUserRoles = DefaultSequenceHandlerUtils.getServiceProviderMappedUserRoles(sequenceConfig,
+                    Arrays.asList(roles));
+            mappedAttrs.put(spRoleUri, spMappedUserRoles);
+        }
+    }
+
+    private String getSpRoleClaimUri(ApplicationConfig appConfig) {
+
+        // get external identity provider role claim uri.
+        String spRoleClaimUri = appConfig.getRoleClaim();
+
+        if (StringUtils.isEmpty(spRoleClaimUri)) {
+            // no role claim uri defined
+            // we can still try to find it out - lets have a look at the claim
+            // mapping.
+            Map<String, String> spToLocalClaimMapping = appConfig.getClaimMappings();
+
+            if (MapUtils.isNotEmpty(spToLocalClaimMapping)) {
+
+                for (Map.Entry<String, String> entry : spToLocalClaimMapping.entrySet()) {
+                    if (FrameworkConstants.LOCAL_ROLE_CLAIM_URI.equals(entry.getValue())) {
+                        spRoleClaimUri = entry.getKey();
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (StringUtils.isEmpty(spRoleClaimUri)) {
+            spRoleClaimUri = FrameworkConstants.LOCAL_ROLE_CLAIM_URI;
+            if (log.isDebugEnabled()) {
+                String serviceProvider = appConfig.getApplicationName();
+                log.debug("Service Provider Role Claim URI not configured for SP: " + serviceProvider +
+                        ". Defaulting to " + spRoleClaimUri);
+            }
+        }
+
+        return spRoleClaimUri;
+    }
+
+    private String getSPStandardDialect(AuthenticationContext context) {
+
+        ApplicationConfig appConfig = context.getSequenceConfig().getApplicationConfig();
+        String spStandardDialect;
+        if (context.getProperties().containsKey(FrameworkConstants.SP_STANDARD_DIALECT)) {
+            spStandardDialect = (String) context.getProperty(FrameworkConstants.SP_STANDARD_DIALECT);
+        } else {
+            spStandardDialect = FrameworkUtils.getStandardDialect(context.getRequestType(), appConfig);
+        }
+        return spStandardDialect;
+    }
+
+    private boolean isLocalClaimDialect(AuthenticationContext context) {
+
+        ApplicationConfig appConfig = context.getSequenceConfig().getApplicationConfig();
+        ClaimConfig claimConfig = appConfig.getServiceProvider().getClaimConfig();
+        return claimConfig.isLocalClaimDialect();
+    }
+
+    private String getStandardRoleClaimURI(String standardDialect, String tenantDomain)
+            throws PostAuthenticationFailedException {
+
+        String roleClaim = getStandardClaimURIFromLocal(standardDialect, tenantDomain, FrameworkConstants
+                .LOCAL_ROLE_CLAIM_URI);
+        if (StringUtils.isBlank(roleClaim)) {
+            return FrameworkConstants.LOCAL_ROLE_CLAIM_URI;
+        }
+        return roleClaim;
+    }
+
+    private String getStandardClaimURIFromLocal(String standardDialect, String tenantDomain, String claimURI)
+            throws PostAuthenticationFailedException {
+
+        try {
+            Map<String, String> claimMapping = ClaimMetadataHandler.getInstance()
+                    .getMappingsMapFromOtherDialectToCarbon(standardDialect, null, tenantDomain, true);
+            if (claimMapping.containsKey(claimURI)) {
+                return claimMapping.get(claimURI);
+            }
+        } catch (ClaimMetadataException e) {
+            throw new PostAuthenticationFailedException(FrameworkErrorConstants.ErrorMessages.
+                    ERROR_WHILE_HANDLING_CLAIM_MAPPINGS.getCode(), FrameworkErrorConstants.ErrorMessages.
+                    ERROR_WHILE_HANDLING_CLAIM_MAPPINGS.getMessage(), e);
+        }
+        return null;
+    }
+
+    private String getSPMappedLocalRoleClaimURI(ApplicationConfig appConfig) {
+
+        String spRoleClaimUri = appConfig.getRoleClaim();
+        if (StringUtils.isNotBlank(spRoleClaimUri)) {
+
+            Map<String, String> spToLocalClaimMapping = appConfig.getClaimMappings();
+            if (spToLocalClaimMapping != null && !spToLocalClaimMapping.isEmpty()) {
+
+                for (Map.Entry<String, String> entry : spToLocalClaimMapping.entrySet()) {
+                    if (spRoleClaimUri.equals(entry.getKey())) {
+                        return entry.getValue();
+                    }
+                }
+            }
+        }
+        return FrameworkConstants.LOCAL_ROLE_CLAIM_URI;
+    }
+
+    private Map<String, String> handleClaimMappings(StepConfig stepConfig, AuthenticationContext context)
+            throws PostAuthenticationFailedException {
+
+        Map<String, String> mappedAttrs;
+        try {
+            mappedAttrs = FrameworkUtils.getClaimHandler().handleClaimMappings(stepConfig, context, null, false);
+            return mappedAttrs;
+        } catch (FrameworkException e) {
+            throw new PostAuthenticationFailedException(FrameworkErrorConstants.ErrorMessages.
+                    ERROR_WHILE_GETTING_CLAIM_MAPPINGS.getCode(), String.format(FrameworkErrorConstants.ErrorMessages.
+                            ERROR_WHILE_GETTING_CLAIM_MAPPINGS.getMessage(),
+                    context.getSequenceConfig().getAuthenticatedUser().getUserName()), e);
+        }
     }
 }
