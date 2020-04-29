78d271b798ed4190ba5ea43ba7fcf5e793f0fe72
GANGANI <gcariyarathne.15@cse.mrt.ac.lk>
2020-04-29 16:40:09 +0530

    Change challenge answer configuration

--- components/identity-event/org.wso2.carbon.identity.event/src/main/java/org/wso2/carbon/identity/event/IdentityEventConstants.java
+++ components/identity-event/org.wso2.carbon.identity.event/src/main/java/org/wso2/carbon/identity/event/IdentityEventConstants.java
@@ -205,6 +205,7 @@ public class IdentityEventConstants {
         public static final String PARAMS = "params";
         public static final String AUTHENTICATION_STATUS = "authenticationStatus";
         public static final String USER_CHALLENGE_ANSWERS = "userChallengeAnswers";
+        public static final String USER_OLD_CHALLENGE_ANSWERS = "userNewChallengeAnswers";
     }
 
     public class ErrorMessage {
